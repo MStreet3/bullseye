80ceb1b3ba0d27a4fe009b73c775a5b757949d09
Michael Street <mi.street@gmail.com>
2020-08-24 15:55:55 -0400

    wip

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var restartIsVisible: Bool = false
+    @State var infoIsVisible: Bool = false
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                Text("100")
+            }
+            
+            HStack {
+                Text("1")
+                Slider(value: .constant(10))
+                Text("100")
+            }
+            
+            HStack {
+                Button(action: {
+                    self.restartIsVisible = true
+                }){
+                    Text("Start Over")
+                }.alert(isPresented: $restartIsVisible){
+                    () -> Alert in return Alert(
+                        title: Text("Start Over"),
+                        message: Text("Are you sure?"),
+                        dismissButton: .default(Text("Start Over")))
+                }
+                Text("Score:")
+                Text("999999")
+                Text("Round:")
+                Text("999")
+                Button(action: {
+                    self.infoIsVisible = true
+                }){
+                    Text("Info")
+                }.alert(isPresented: $infoIsVisible){
+                    () -> Alert in return Alert(
+                        title: Text("Information"),
+                        message: Text("You suck."),
+                        dismissButton: .default(Text("Thank You!")))
+                }
+            }
+            
+            
+            
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414
+            ))
     }
 }
