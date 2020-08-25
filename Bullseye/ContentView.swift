089875c127df750e8ba31ab08fff39f6ca2269b1
Michael Street <mi.street@gmail.com>
2020-08-25 15:26:50 -0400

    app functionality

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,24 +10,49 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var alertIsVisible: Bool = false
     @State var restartIsVisible: Bool = false
     @State var infoIsVisible: Bool = false
-    
+    @State var sliderValue: Double = 50.0
+    @State var target: Int = Int.random(in: 1...100)
+    @State var score: Int = 0
+    @State var round: Int = 1
     
     var body: some View {
         VStack{
-            
+            Spacer()
             HStack {
                 Text("Put the bullseye as close as you can to:")
-                Text("100")
+                Text("\(self.target)")
             }
-            
+            Spacer()
             HStack {
                 Text("1")
-                Slider(value: .constant(10))
+                Slider(value: $sliderValue, in: 1...100)
                 Text("100")
             }
-            
+            Button(action: {
+                self.alertIsVisible = true
+                
+            }){
+                Text("Hit Me")
+            }.alert(isPresented: $alertIsVisible){
+                () -> Alert in
+                let roundedValue: Int = Int(self.sliderValue.rounded())
+                let currentScore = self.pointsForCurrentRound(value: roundedValue, target: self.target)
+               
+                return Alert(
+                    title: Text("\(self.alertTitle(score: currentScore))"),
+                    message: Text("The slider value is \(roundedValue).\n" +
+                        "You scored \(currentScore) points this round."),
+                    dismissButton: .default(Text("Thank You!")){
+                        let scoreForRound = self.pointsForCurrentRound(value: Int(self.sliderValue.rounded()), target: self.target)
+                        self.score += scoreForRound
+                        self.target = Int.random(in: 1...100)
+                        self.round += 1
+                    })
+            }
+            Spacer()
             HStack {
                 Button(action: {
                     self.restartIsVisible = true
@@ -37,12 +62,19 @@ struct ContentView: View {
                     () -> Alert in return Alert(
                         title: Text("Start Over"),
                         message: Text("Are you sure?"),
-                        dismissButton: .default(Text("Start Over")))
+                        dismissButton: .default(Text("Start Over")){
+                            self.score = 0
+                            self.target = Int.random(in: 1...100)
+                            self.round = 1
+                        })
                 }
+                Spacer()
                 Text("Score:")
-                Text("999999")
+                Text("\(self.score)")
+                Spacer()
                 Text("Round:")
-                Text("999")
+                Text("\(self.round)")
+                Spacer()
                 Button(action: {
                     self.infoIsVisible = true
                 }){
@@ -53,12 +85,33 @@ struct ContentView: View {
                         message: Text("You suck."),
                         dismissButton: .default(Text("Thank You!")))
                 }
-            }
-            
-            
-            
+            }.padding(.bottom, 20)
         }
-        
+    }
+    func pointsForCurrentRound(value: Int, target: Int) -> Int {
+        let points = 100 - abs(target - value)
+        switch points {
+        case 100:
+            return 200
+        case 99:
+            return 150
+        default:
+            return points
+        }
+    }
+    func alertTitle(score: Int) -> String {
+        switch score {
+        case 0...50:
+            return "Terrible."
+        case 51...75:
+            return "Close!"
+        case 75...98:
+            return "Awesome!"
+        case 100:
+            return "Perfect!"
+        default:
+            return "Try again!"
+        }
     }
 }
 
